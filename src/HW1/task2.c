4d1b353321b899f0cb3f7f4bae6c9d18481658cd
Dev Spain <9984398@gmail.com>
2022-08-13 22:11:48 +0300

    task2 : Completed.

---
+++ src/HW1/task2.c
@@ -0,0 +1,33 @@
+//
+// Created by Dmitry Morozov on 13/8/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main () {
+    int a = 5, c = 5;
+
+    int b;
+    printf (" b = ");
+    scanf ("%d", &b);
+
+    int d;
+    printf (" d = ");
+    scanf ("%d", &d);
+
+    a = a + b - 2;
+    c = c + 1;
+    d = c - a + d;
+    a = a * c;
+    c = c - 1;
+    a = a / 10;
+    c = c / 2;
+    b = b - 1;
+    d = d * (c + b + a);
+
+    printf (" a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+    return 0;
+}
