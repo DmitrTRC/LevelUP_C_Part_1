c3bb90c18a412f4f82321ff5de95fc119774cebe
Dev Spain <9984398@gmail.com>
2022-08-23 00:48:03 +0300

    Task1 : Tests : Added test cases

--- HW2/tests/test_task1.cpp
+++ HW2/tests/test_task1.cpp
@@ -15,6 +15,11 @@ TEST(Task1, getEquation) {
     EXPECT_EQ(getEquation (1), 0 );
     EXPECT_NEAR(getEquation (2), 499.641, PRECISSION);
     EXPECT_NEAR(getEquation (3), -2066.53, PRECISSION);
+
+    double nan = std::numeric_limits<double>::quiet_NaN();
+    double inf = std::numeric_limits<double>::infinity();
+    ASSERT_NE(getEquation (-1), nan);
+    EXPECT_EQ(getEquation (0), inf);
 }
 
 
