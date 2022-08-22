90346844a340bfdaba52a5d0bc312d26f2b64723
Dev Spain <9984398@gmail.com>
2022-08-23 00:49:37 +0300

    Task1 : Tests : Names : HotFix

--- HW2/tests/test_task1.cpp
+++ HW2/tests/test_task1.cpp
@@ -7,14 +7,14 @@
 #include "Task1.hpp"
 
 
-const double PRECISSION = .01;
+const double EPSILON = .01;
 
 
 TEST(Task1, getEquation) {
 
     EXPECT_EQ(getEquation (1), 0 );
-    EXPECT_NEAR(getEquation (2), 499.641, PRECISSION);
-    EXPECT_NEAR(getEquation (3), -2066.53, PRECISSION);
+    EXPECT_NEAR(getEquation (2), 499.641, EPSILON);
+    EXPECT_NEAR(getEquation (3), -2066.53, EPSILON);
 
     double nan = std::numeric_limits<double>::quiet_NaN();
     double inf = std::numeric_limits<double>::infinity();
