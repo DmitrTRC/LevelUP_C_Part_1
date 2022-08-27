d81fb16315197778438a7dbdb0716171a7f2605d
Dev Spain <9984398@gmail.com>
2022-08-27 15:40:55 +0300

    Code structure : Fixed

---
+++ Playground/src/sequence_sum.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Dmitry Morozov on 27/8/22.
+//
+
+#include "sequence_sum.hpp"
+
+#include <cmath>
+#include <functional>
+
+
+// Calculate sum of number sequence f(x) = x/ 2 + 2/3 x^2 + 3/4 x^3 + ... + (n - 1) / n x^n Epsilion = 0.00001
+// for x from 0 to n
+
+double sequence_sum (double x) {
+
+    std::function<double (double, int)> get_next = [ &get_next ] (double x, int n) -> double {
+        return pow (x, n) * ((double) n / (n + 1));
+    };
+
+    double epsilon = 1e-5;
+    double acc = 0;
+    int iteration = 0;
+    double next_term = get_next (x, 1);
+    do {
+        ++iteration;
+        acc += next_term;
+        next_term = get_next (x, iteration);
+
+    } while (next_term > epsilon);
+
+    return acc;
+}
---
+++ Playground/src/sequence_sum.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Dmitry Morozov on 27/8/22.
+//
+
+#ifndef LEVELUP_C_PARTI_MASTER_SEQUENCE_SUM_HPP
+#define LEVELUP_C_PARTI_MASTER_SEQUENCE_SUM_HPP
+
+double sequence_sum (double);
+
+#endif //LEVELUP_C_PARTI_MASTER_SEQUENCE_SUM_HPP
