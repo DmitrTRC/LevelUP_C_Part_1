ddec70d71e0d9fc2cae5a4256f5a16c29adc16b8
Dev Spain <9984398@gmail.com>
2022-08-27 20:40:07 +0300

    HomeWork_3_3 : Tests : Passed

--- HomeWork_3_3/tests/test_task3.cpp
+++ HomeWork_3_3/tests/test_task3.cpp
@@ -7,3 +7,17 @@
 #include "Task3.hpp"
 
 
+TEST(Task3, swap_first_last_digits) {
+    EXPECT_EQ(swap_first_last_digits (1), 1);
+    EXPECT_EQ(swap_first_last_digits (12), 21);
+    EXPECT_EQ(swap_first_last_digits (123), 321);
+    EXPECT_EQ(swap_first_last_digits (1234), 4231);
+    EXPECT_EQ(swap_first_last_digits (6789), 9786);
+    EXPECT_EQ(swap_first_last_digits (12345), 52341);
+    EXPECT_EQ(swap_first_last_digits (123456), 623451);
+    EXPECT_EQ(swap_first_last_digits (1234567), 7234561);
+    EXPECT_EQ(swap_first_last_digits (12345678), 82345671);
+    EXPECT_EQ(swap_first_last_digits (123456789), 923456781);
+
+
+}
