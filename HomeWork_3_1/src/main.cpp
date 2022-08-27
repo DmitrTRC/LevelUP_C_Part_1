637e4e5c5fe092f90a7e09026252a9916696ff3e
Dev Spain <9984398@gmail.com>
2022-08-27 18:55:47 +0300

    HomeWork_3_1 : HotFix

--- HomeWork_3_1/src/main.cpp
+++ HomeWork_3_1/src/main.cpp
@@ -11,7 +11,10 @@ int main (int argc, char **argv) {
 
 
     std::cout << "HOME WORK 3\n\nTask 1" << std::endl;
-
+    std::cout << "Enter natural N  : ";
+    int n;
+    std::cin >> n;
+    std::cout << "The length of the number is " << getNaturalLength (n) << std::endl;
 
     return 0;
 }
---
+++ HomeWork_3_2/docs/Task_3_2.md
@@ -0,0 +1,3 @@
+### Find The Least Common Divisor
+
+* Include 1 if not any other divisor is found
