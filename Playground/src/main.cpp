a80921af5d3387cd57f8c4600c18369891152f31
Dev Spain <9984398@gmail.com>
2022-08-27 15:32:58 +0300

    Refactored

--- Playground/src/main.cpp
+++ Playground/src/main.cpp
@@ -2,31 +2,40 @@
 // Created by Dmitry Morozov on 27/8/22.
 //
 
-#include <iostream>
-#include <cstdio>
 #include <cmath>
+#include <cstdio>
+#include <functional>
 
 
 // Calculate sum of number sequence f(x) = x/ 2 + 2/3 x^2 + 3/4 x^3 + ... + (n - 1) / n x^n Epsilion = 0.00001
 // for x from 0 to n
+
+double sequence_sum (double x) {
+
+    std::function<double (double, int)> get_next = [ &get_next ] (double x, int n) -> double {
+        return pow (x, n) * ((double) n / (n + 1));
+    };
+
+    double epsilon = 1e-5;
+    double acc = 0;
+    int iteration = 0;
+    double next_term = get_next (x, 1);
+    do {
+        ++iteration;
+        acc += next_term;
+        next_term = get_next (x, iteration);
+
+    } while (next_term > epsilon);
+
+    return acc;
+}
+
+
 int main (int argc, char **argv) {
     double sum = 0;
     double x;
     printf ("x = ");
     scanf ("%lf", &x);
-    double eps = 1e-5;
-    int n{2};
-    do {
-        auto tmp = ((n - 1) / double (n)) * pow (x, n - 1);
-        sum += tmp;
-        if (tmp < eps) {
-            break;
-        }
+    printf ("Sum = %lf\n", sequence_sum (x));
 
-        ++n;
-    } while (true);
-    printf ("sum = %lf\n", sum);
-
-
-    return 0;
 }
