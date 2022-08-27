65f1c2818b227e303585ae81dad6ee0cc79f43d3
Dev Spain <9984398@gmail.com>
2022-08-27 14:04:06 +0300

    Math sequence : Done Tested : None

--- Playground/src/main.cpp
+++ Playground/src/main.cpp
@@ -3,11 +3,30 @@
 //
 
 #include <iostream>
+#include <cstdio>
+#include <cmath>
 
 
+// Calculate sum of number sequence f(x) = x/ 2 + 2/3 x^2 + 3/4 x^3 + ... + (n - 1) / n x^n Epsilion = 0.00001
+// for x from 0 to n
 int main (int argc, char **argv) {
+    double sum = 0;
+    double x;
+    printf ("x = ");
+    scanf ("%lf", &x);
+    double eps = 1e-5;
+    int n{2};
+    do {
+        auto tmp = ((n - 1) / double (n)) * pow (x, n - 1);
+        sum += tmp;
+        if (tmp < eps) {
+            break;
+        }
 
-    std::cout << "Hello, World!" << std::endl;
+        ++n;
+    } while (true);
+    printf ("sum = %lf\n", sum);
+
 
     return 0;
 }
