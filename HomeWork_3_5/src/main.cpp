0d7f7f6ebeda3e7c83801025d2d91fdf60a9f60f
Dev Spain <9984398@gmail.com>
2022-08-30 02:40:10 +0300

    HotFix : main.cpp : Limits conditions : Done

--- HomeWork_3_5/src/main.cpp
+++ HomeWork_3_5/src/main.cpp
@@ -17,7 +17,7 @@ int main (int argc, char **argv) {
     std::cout << "Enter x in range [0.05, 1.0] : ";
     double x;
     std::cin >> x;
-    if (x < LOWER_LIMIT || x > UPPER_LIMIT) {
+    if (x <= LOWER_LIMIT || (x >= UPPER_LIMIT)) {
         std::cout << "Error: x is not in range [0.05, 1.0]" << std::endl;
         return 1;
     }
