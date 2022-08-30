fb79c53b1bfc4c943b9d2731ae9f6da3ebd23830
Dev Spain <9984398@gmail.com>
2022-08-30 08:53:37 +0300

    task5 : main() : Refactored

--- HomeWork_3_5/src/main.cpp
+++ HomeWork_3_5/src/main.cpp
@@ -10,18 +10,19 @@
 int main (int argc, char **argv) {
     const double UPPER_LIMIT = 1.0;
     const double LOWER_LIMIT = 0.05;
-    const double EPS = 0.00001;
 
     std::cout << "HOME WORK 3\n\nTask 5" << std::endl;
 
     std::cout << "Enter x in range [0.05, 1.0] : ";
     double x;
     std::cin >> x;
+
     if (x <= LOWER_LIMIT || (x >= UPPER_LIMIT)) {
-        std::cout << "Error: x is not in range [0.05, 1.0]" << std::endl;
+        std::cout << "Error: x is not in range ]0.05, 1.0[" << std::endl;
         return 1;
     }
-    std::cout << "The value of the function is : " << reduceSequence (x) << std::endl;
+
+    std::cout << "The sum of series : " << reduceSequence (x) << std::endl;
 
     return 0;
 }
