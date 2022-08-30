fe8c3e8c7200bc56dc8c752535ab330f3a537876
Dev Spain <9984398@gmail.com>
2022-08-30 21:04:31 +0300

    HotFix : UPPER condition for x : Fixed

--- HomeWork_3_5/src/main.cpp
+++ HomeWork_3_5/src/main.cpp
@@ -18,7 +18,7 @@ int main (int argc, char **argv) {
     double x;
     std::cin >> x;
 
-    if (x <= LOWER_LIMIT || (x > UPPER_LIMIT)) { //!!!! >=
+    if (x <= LOWER_LIMIT || (x >= UPPER_LIMIT)) {
         std::cout << "Error: x is not in range ]0.05, 1.0[" << std::endl;
         return 1;
     }
