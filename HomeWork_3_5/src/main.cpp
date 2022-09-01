0290a07877c37110c0b6ee52e74dc97c8210121b
Dmitry Morozov <9984398@gmail.com>
2022-09-02 00:38:18 +0300

    Task5 : main() : Refactored

--- HomeWork_3_5/src/main.cpp
+++ HomeWork_3_5/src/main.cpp
@@ -12,21 +12,19 @@ int main (int argc, char **argv) {
     const double UPPER_LIMIT = 1.0;
     const double LOWER_LIMIT = 0.05;
 
-//    std::cout << "HOME WORK 3\n\nTask 5" << std::endl;
-//
-//    std::cout << "Enter x in range ]0.05, 1.0[ : ";
-//    double x;
-//    std::cin >> x;
-//
-//    if (x <= LOWER_LIMIT || (x >= UPPER_LIMIT)) {
-//        std::cout << "Error: x is not in range ]0.05, 1.0[" << std::endl;
-//        return 1;
-//    }
-//
-//    std::cout << "The sum of series : " << std::setprecision (6) << reduceSequence (x) << std::endl;
-    std::cout << "dFact(0) : " << test_factorial (0) << std::endl;
-    std::cout << "dFact(1) : " << test_factorial (1) << std::endl;
-    std::cout << "dFact(-1) : " << test_factorial (-1) << std::endl;
+    std::cout << "HOME WORK 3\n\nTask 5" << std::endl;
+
+    std::cout << "Enter x in range ]0.05, 1.0[ : ";
+    double x;
+    std::cin >> x;
+
+    if (x <= LOWER_LIMIT || (x >= UPPER_LIMIT)) {
+        std::cout << "Error: x is not in range ]0.05, 1.0[" << std::endl;
+        return 1;
+    }
+
+    std::cout << "The sum of series : " << std::setprecision (6) << reduceSequence (x) << std::endl;
+
+
     return 0;
 }
-
--- Playground/src/main.cpp
+++ Playground/src/main.cpp
@@ -1,16 +1,46 @@
-#include <iostream>
+#include <math.h>
+#include <stdio.h>
+//Сумма рядка с точностью 10^-5, где 0.05<x<1
 
+#define INPUT_MIN 0.05
+#define INPUT_MAX 1
 
-// Swap last and first elements of number. 6789 -> 9786
-int main () {
-    int n;
-
-
-    int array[6] = {6, 7, 8, 9, 0, 1};
-    int tmp = array[0];
-    array[0] = array[5];
-    array[5] = tmp;
-
+int inputValid (double x) {
+    if (INPUT_MIN < x && x < INPUT_MAX) {
+        return 1;
+    }
     return 0;
+}
 
+int main () {
+    printf ("Enter number x, which is more than 0.05 and less than 1: ");
+    double x;
+    scanf ("%lf", &x);
+
+    if (!inputValid (x)) {
+        printf ("You have entered wrong number!");
+        return EXIT_FAILURE;
+    }
+
+    double row = 1;
+    double sum = x;
+
+    int fact1;
+    int fact2;
+
+    for (int i = 1; row > 0.00001; ++i) {
+        fact1 = 1;
+        fact1 *= (2 * i - 1);
+
+        fact2 = 1;
+        fact2 *= 2 * i;
+
+        row *= (fact1 * pow (x, (2 * i + 1))) / (fact2 * (2 * i + 1));
+
+        sum += row;
+    }
+
+    printf ("Sum is: %lf\n", sum);
+
+    return EXIT_SUCCESS;
 }
