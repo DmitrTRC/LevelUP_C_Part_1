5836bea6d180b835ebfd5fa66396a88da1825f78
Dmitry Morozov <9984398@gmail.com>
2022-09-10 11:04:22 +0300

    Doxy fixed.

--- Playground/src/main.cpp
+++ Playground/src/main.cpp
@@ -1,52 +1,32 @@
-#include <iostream>
+/*
+    A character string is given. Determine if it is a valid parentheses expression.
+*/
 
-
-
-//Определить местоположение элементов массива А(30), не встречающихся в массиве В(15)
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 
 int main () {
-    constexpr int size_arr_a = 30;
-    int A[size_arr_a];
-
-
-    srand (time (nullptr));
-
-    printf ("A: ");
-    for (int i = 0; i < size_arr_a; ++i) {
-        A[i] = rand () % 21 - 5;
-        printf ("%d ", A[i]);
+    constexpr int s = 40;
+    char M[s];
+    gets (M);
+    int n = 0;
+    int i = 0;
+    while (M[i] != 0) {
+        if (M[i] == '(') {
+            ++n;
+        }
+        if (M[0] == ')') {
+            printf ("incorrect brackets");
+            return 1;
+        } else if (M[i] == ')') {
+            --n;
+        }
+        ++i;
     }
-
-    {
-        constexpr int size_arr_b = 15;
-        int B[size_arr_b];
-
-        srand (time (nullptr));
-
-        printf ("B: ");
-        for (int i = 0; i < size_arr_b; ++i) {
-            B[i] = rand () % 21 - 5;
-            printf ("%d ", B[i]);
-        }
-
-        for (int x = 0; x < size_arr_a; ++x) {
-            bool isFound = false;
-
-            for (int y = 0; y < size_arr_b; ++y) {
-                if (A[x] == B[y]) {
-                    isFound = true;
-                    break;
-                }
-            }
-            if (!isFound) {
-                printf ("A[%d] = %d", x, A[x]);
-
-            }
-
-        }
-        return 0;
+    if (n == 0) {
+        printf ("correct parentheses");
+    } else {
+        printf ("incorrect brackets");
+    }
+    return 0;
 }
