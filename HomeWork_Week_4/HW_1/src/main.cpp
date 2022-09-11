131338e47d969e400ec22e903bca19f41bc4b580
Dmitry Morozov <9984398@gmail.com>
2022-09-11 09:35:09 +0300

    Module 4 : HW_1 : Started

---
+++ HomeWork_Week_4/HW_1/src/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Dmitry Morozov on 21/8/22.
+//
+
+
+
+#include <iostream>
+
+
+/**
+ * @brief Main function
+ * @return 0
+ */
+int main () {
+    std::cout << "HOME WORK x\n\nTask x" << std::endl;
+
+
+    return 0;
+}
+
---
+++ HomeWork_Week_4/HW_1/tests/xxx_test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Dmitry Morozov on 27/8/22.
+//
+
+#include "gtest/gtest.h"
+
+#include "shared/lcd.hpp"
+
+
+TEST (Taskx, xxx) {
+    EXPECT_EQ (1, 1);
+
+}
