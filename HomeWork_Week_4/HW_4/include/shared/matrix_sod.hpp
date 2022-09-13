81c6f161c143afef513f29b8fa741707f5931185
Dmitry Morozov <9984398@gmail.com>
2022-09-14 00:06:41 +0300

    Tests: HomeWork Week 4 HW_4 : Done Code refactoring : Done Tests : PASSED

--- HomeWork_Week_4/HW_4/include/shared/matrix_sod.hpp
+++ HomeWork_Week_4/HW_4/include/shared/matrix_sod.hpp
@@ -5,24 +5,45 @@
 #ifndef LEVELUP_C_PARTI_DEV_TASK1_HPP
 #define LEVELUP_C_PARTI_DEV_TASK1_HPP
 
-#include <array>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+
+template<typename T>
+void printMatrix (std::vector<std::vector<T>> &matrix) {
+    for (auto &row: matrix) {
+        for (auto &element: row) {
+            std::cout << std::setw (4) << element;
+        }
+        std::cout << std::endl;
+    }
+}
 
 /**
  * @brief Compute the sum of the 2d-matrix elements to the left of the side diagonal
  * @tparam T
- * @tparam SIZE
  * @param matrix
  * @return T
  */
-template<typename T, std::size_t SIZE>
-T sumMatrixSideDiagonalElements (std::array<std::array<T, SIZE>, SIZE> &matrix) {
-    T sum = 0;
-    for (std::size_t i = 0; i < SIZE; ++i) {
-        for (std::size_t j = 0; j < SIZE; ++j) {
-            if (i + j < SIZE - 1) {
+template<typename T>
+T sumMatrixSideDiagonalElements (std::vector<std::vector<T>> &matrix) {
+    T sum{};
+
+    if (matrix.empty ()) {
+        return sum;
+    } else if (matrix.size () != matrix[0].size ()) {
+        throw std::invalid_argument ("Matrix must be square");
+    }
+
+
+    for (size_t i = 0; i < matrix.size (); ++i) {
+        for (size_t j = 0; j < matrix[i].size (); ++j) {
+            if (i + j < matrix.size () - 1) {
                 sum += matrix[i][j];
             }
         }
+
     }
 
     return sum;
